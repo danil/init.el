67121d8cf2c4a443f82c4f45f1f22e62f0f15520
Danil Kutkevich <danil@kutkevich.org>
2015-11-12 20:04:43 +0300

    fix: ethan-wspace-mode respects Makefile, C and Go
    ethan-wspace-mode respects tabs in Makefile, C and Go.

--- recipes/init-ethan-wspace.el
+++ recipes/init-ethan-wspace.el
@@ -1,3 +1,38 @@
+;;; init-ethan-wspace.el --- This file is part of Danil <danil@kutkevich.org> home.
+
+;; Copyright (C) 2015 Danil <danil@kutkevich.org>.
+;; Author: Danil <danil@kutkevich.org>
+;; Version: 0.0.1
+;; Package-Requires: ((my-init))
+;; Keywords: convenience
+;; URL: https://github.com/danil/init.el
+
+;;; Commentary:
+
+;; Please see README.md for documentation.
+
+;;; License:
+
+;; This file is not part of GNU Emacs.
+;; However, it is distributed under the same license.
+
+;; GNU Emacs is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+
+;; GNU Emacs is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to the
+;; Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+;; Boston, MA 02110-1301, USA.
+
+;;; Code:
+
 (my-init--hook
   (global-ethan-wspace-mode t)
 
@@ -6,4 +41,14 @@
     ;; (setq  ethan-wspace-mode-line-element nil) ;typically looks like: ew:tLNm
 
     (cond ((equal frame-background-mode 'dark)
-           (set-face-background 'ethan-wspace-face "gray15")))))
+           (set-face-background 'ethan-wspace-face "gray15")))
+
+    (my-init--add-mode-to-hooks (lambda () (setq ethan-wspace-errors
+                                            (remove 'tabs ethan-wspace-errors)))
+
+                                '(c-mode-hook
+                                  go-mode-hook
+                                  makefile-gmake-mode-hook
+                                  makefile-mode-hook))))
+
+;;; init-ethan-wspace.el ends here
