db34620df07e0f4328d6ca4748f2c4ed8bb143f0
Danil Kutkevich <danil@kutkevich.org>
2016-01-14 16:55:21 +0300

    fix: deft init config

--- recipes/init-deft.el
+++ recipes/init-deft.el
@@ -1,11 +1,53 @@
+;;; init-deft.el --- This file is part of Danil <danil@kutkevich.org> home.
+
+;; Copyright (C) 2015 Danil <danil@kutkevich.org>.
+;; Author: Danil <danil@kutkevich.org>
+;; Version: 0.0.1
+;; Package-Requires: ((my-init))
+;; Keywords: convenience
+;; URL: https://github.com/danil/init.el
+
+;;; Commentary:
+
+;; Please see README.md for documentation.
+
+;;; License:
+
+;; This file is not part of GNU Emacs.
+;; However, it is distributed under the same license.
+
+;; GNU Emacs is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+
+;; GNU Emacs is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to the
+;; Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+;; Boston, MA 02110-1301, USA.
+
+;;; Code:
+
+(custom-set-variables
+ '(setq deft-directory "~/Dropbox/deft") ;<http://jblevins.org/projects/deft>
+ '(setq deft-extension "md")
+ '(setq deft-text-mode 'markdown-mode))
+
 (my-init--hook
   (global-set-key (my-kbd "x d") 'my-deft)
 
   (my-init--after-load 'deft
-    (setq deft-directory "~/Dropbox/notes") ;<http://jblevins.org/projects/deft>
-    (setq deft-text-mode 'markdown-mode)
-    (setq deft-extension "md")))
+    (define-key deft-mode-map (kbd "C-c C-f") nil)))
 
 (defun my-deft (&optional arg)
+  "Open `deft' list or create `deft' item if `ARG'."
+
   (interactive "P")
   (if arg (deft-new-file) (deft)))
+
+;;; init-deft.el ends here
