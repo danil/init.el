eb7ba8732736e83f6f8ca7a8d6a6130fe230c1c4
Danil Kutkevich <danil@kutkevich.org>
2016-01-14 20:23:56 +0300

    feat: conf-mode for .stalonetrayrc file

--- recipes/init-conf-mode.el
+++ recipes/init-conf-mode.el
@@ -1,3 +1,38 @@
+;;; init-conf-mode.el --- This file is part of Danil <danil@kutkevich.org> home.
+
+;; Copyright (C) 2016 Danil <danil@kutkevich.org>.
+;; Author: Danil <danil@kutkevich.org>
+;; Version: 0.0.1
+;; Package-Requires: ((my-init))
+;; Keywords: convenience
+;; URL: https://github.com/danil/init.el
+
+;;; Commentary:
+
+;; Please see README.md for documentation.
+
+;;; License:
+
+;; This file is not part of GNU Emacs.
+;; However, it is distributed under the same license.
+
+;; GNU Emacs is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+
+;; GNU Emacs is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to the
+;; Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+;; Boston, MA 02110-1301, USA.
+
+;;; Code:
+
 (my-init--hook
   (my-init--add-mode-to-patterns 'conf-mode
                                 "/\\.config/skippy-xd/skippy-xd\\.rc\\'"
@@ -34,6 +69,7 @@
                                 "\\.gtkrc.mine\\'"
                                 "\\.pkla\\'"
                                 "\\.skippyrc\\'"
+                                "\\.stalonetrayrc\\'"
                                 "\\.theme\\'"
                                 "\\.xxkbrc\\'"
                                 "\\parcelliterc\\'"
@@ -65,3 +101,5 @@
   (my-init--add-mode-to-patterns 'conf-colon-mode
                                 "/rsyncd.secrets\\'"
                                 "\\.pgpass\\'"))
+
+;;; init-conf-mode.el ends here
