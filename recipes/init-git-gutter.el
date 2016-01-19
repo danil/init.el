baed7d868448ddaab498e06764a8b5386f3d96d7
Danil Kutkevich <danil@kutkevich.org>
2016-01-19 15:47:02 +0300

    minor

--- recipes/init-git-gutter.el
+++ recipes/init-git-gutter.el
@@ -1,8 +1,43 @@
-(global-set-key (my-kbd "v h k") 'git-gutter:revert-hunk)
-(global-set-key (my-kbd "v h n") 'my-git-gutter-next-diff)
-(global-set-key (my-kbd "v h p") 'my-git-gutter-previous-diff)
+;;; init-git-gutter.el --- This file is part of Danil <danil@kutkevich.org> home.
+
+;; Copyright (C) 2016 Danil <danil@kutkevich.org>.
+;; Author: Danil <danil@kutkevich.org>
+;; Version: 0.0.1
+;; Package-Requires: ((my-init))
+;; Keywords: convenience
+;; URL: https://github.com/danil/init.el
+
+;;; Commentary:
+
+;; Please see README.md for documentation.
+
+;;; License:
+
+;; This file is not part of GNU Emacs.
+;; However, it is distributed under the same license.
+
+;; GNU Emacs is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+
+;; GNU Emacs is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to the
+;; Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+;; Boston, MA 02110-1301, USA.
+
+;;; Code:
 
 (my-init--hook
+  (global-set-key (my-kbd "v h k") 'git-gutter:revert-hunk)
+  (global-set-key (my-kbd "v h n") 'my-git-gutter-next-diff)
+  (global-set-key (my-kbd "v h p") 'my-git-gutter-previous-diff)
+
   ;; (my-init--after-load 'git-gutter
   ;;   (setq git-gutter:disabled-modes '(shell-mode magit-mode)))
 
@@ -27,3 +62,5 @@
   (interactive)
   (my-with-repeat-while-press-last-key
     (call-interactively 'git-gutter:previous-hunk)))
+
+;;; init-git-gutter.el ends here
