b735fa8a7409505a0a3980a9ebad28dfd7567a96
Danil Kutkevich <danil@kutkevich.org>
2016-01-29 18:38:47 +0300

    minor

--- recipes/init-yasnippet.el
+++ recipes/init-yasnippet.el
@@ -1,3 +1,40 @@
+;;; init-yasnippet.el --- This file is part of Danil <danil@kutkevich.org> home.
+
+;; Copyright (C) 2016 Danil <danil@kutkevich.org>.
+;; Author: Danil <danil@kutkevich.org>
+;; Version: 0.0.1
+;; Package-Requires: ((my-init))
+;; Keywords: convenience
+;; URL: https://github.com/danil/init.el
+
+;;; Commentary:
+
+;; Please see README.md for documentation.
+
+;;; License:
+
+;; This file is not part of GNU Emacs.
+;; However, it is distributed under the same license.
+
+;; GNU Emacs is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+
+;; GNU Emacs is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to the
+;; Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+;; Boston, MA 02110-1301, USA.
+
+;;; Code:
+
+;;; <http://pragmaticemacs.com/emacs/smart-text-templates-with-yasnippet/>
+
 (autoload 'yas-reload-all "yasnippet" nil t)
 
 (my-init--hook
@@ -12,3 +49,5 @@
     (define-key yas-minor-mode-map (kbd "<backtab>") 'yas-expand))
 
   (my-init--add-mode-to-hooks 'yas-minor-mode my-init--programming-modes-hooks))
+
+;;; init-yasnippet.el ends here
