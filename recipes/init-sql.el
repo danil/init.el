f16ba2f907f34ef4d843ea1a16855bd3cee8eebb
Danil Kutkevich <danil@kutkevich.org>
2016-02-04 16:18:01 +0300

    feat: add interactive sql shell cleanup function

--- recipes/init-sql.el
+++ recipes/init-sql.el
@@ -34,20 +34,41 @@
 ;;; Code:
 
 (my-init--hook
-  ;; Sql mode history <http://www.emacswiki.org/emacs/SqlMode#toc3>.
-  (defun my-sql-save-history-hook ()
-    (let ((lval 'sql-input-ring-file-name)
-          (rval 'sql-product))
-      (if (symbol-value rval)
-          (let ((filename
-                 (concat "~/.emacs.d/sql/"
-                         (symbol-name (symbol-value rval))
-                         "-history.sql")))
-            (set (make-local-variable lval) filename))
-        (error
-         (format "SQL history will not be saved because %s is nil"
-                 (symbol-name rval))))))
+  (add-hook 'sql-interactive-mode-hook 'my-sql-save-history-hook)
 
-  (add-hook 'sql-interactive-mode-hook 'my-sql-save-history-hook))
+  (my-init--after-load 'sql
+    (define-key sql-interactive-mode-map (my-kbd "C-l") 'my-sql-shell-clear)))
+
+(defun my-sql-shell-clear (&optional arg)
+  "Delete output from `SQl' shell or kill output from `SQL' shell if `ARG'.
+Clear current `SQL' shell beginning with a-la prompt to `point-min'.
+Clearing by `delete-region' or by `kill-region' if `ARG'."
+
+  (interactive "P")
+  (goto-char (point-max))
+  (search-backward "=#")
+  (beginning-of-line)
+
+  (if arg
+      (kill-region (point-min) (point))
+    (delete-region (point-min) (point)))
+
+  (delete-region (point-min) (point))
+
+  (goto-char (point-max)))
+
+;; Sql mode history <http://www.emacswiki.org/emacs/SqlMode#toc3>.
+(defun my-sql-save-history-hook ()
+  (let ((lval 'sql-input-ring-file-name)
+        (rval 'sql-product))
+    (if (symbol-value rval)
+        (let ((filename
+               (concat "~/.emacs.d/sql/"
+                       (symbol-name (symbol-value rval))
+                       "-history.sql")))
+          (set (make-local-variable lval) filename))
+      (error
+       (format "SQL history will not be saved because %s is nil"
+               (symbol-name rval))))))
 
 ;;; init-sql.el ends here
