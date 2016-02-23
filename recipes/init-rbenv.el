e6a78573fd7c4736abd025da89a773a5ce501296
Danil Kutkevich <danil@kutkevich.org>
2016-02-23 23:22:15 +0300

    fix: rbenv init respects `.ruby-version` file

--- recipes/init-rbenv.el
+++ recipes/init-rbenv.el
@@ -1,9 +1,48 @@
+;;; init-rbenv.el --- This file is part of Danil <danil@kutkevich.org> home.
+
+;; Copyright (C) 2016 Danil <danil@kutkevich.org>.
+;; Author: Danil <danil@kutkevich.org>
+;; Version: 0.0.1
+;; Package-Requires: ((my-init))
+;; Keywords: convenience
+;; URL: https://github.com/danil/init.el
+
+;;; Commentary:
+
+;; Please see README.md for documentation.
+
+;;; License:
+
+;; This file is not part of GNU Emacs.
+;; However, it is distributed under the same license.
+
+;; GNU Emacs is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+
+;; GNU Emacs is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to the
+;; Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+;; Boston, MA 02110-1301, USA.
+
+;;; Code:
+
 ;; Ruby with Emacs and Rbenv
 ;; <http://blog.senny.ch/blog/2013/02/11/use-the-right-ruby-with-emacs-and-rbenv>.
 (custom-set-variables '(rbenv-modeline-function 'my-rbenv--modeline-with-face))
 
-(my-init--hook (global-rbenv-mode))
+(my-init--hook
+  (global-rbenv-mode)
+  (rbenv-use-corresponding))
 
 (defun my-rbenv--modeline-with-face (current-ruby)
   (unless (equal current-ruby "system")
     (list (propertize current-ruby 'face 'rbenv-active-ruby-face))))
+
+;;; init-rbenv.el ends here
