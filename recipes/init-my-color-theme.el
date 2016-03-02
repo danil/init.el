9e09630a0aedeb138c41c4802009fb2849add1af
Danil Kutkevich <danil@kutkevich.org>
2016-03-02 13:18:51 +0300

    fix: shut up annoying yellow sh-heredoc face

--- recipes/init-my-color-theme.el
+++ recipes/init-my-color-theme.el
@@ -1,3 +1,38 @@
+;;; init-my-color-theme.el --- This file is part of Danil <danil@kutkevich.org> home.
+
+;; Copyright (C) 2016 Danil <danil@kutkevich.org>.
+;; Author: Danil <danil@kutkevich.org>
+;; Version: 0.0.1
+;; Package-Requires: ((my-init))
+;; Keywords: convenience
+;; URL: https://github.com/danil/init.el
+
+;;; Commentary:
+
+;; Please see README.md for documentation.
+
+;;; License:
+
+;; This file is not part of GNU Emacs.
+;; However, it is distributed under the same license.
+
+;; GNU Emacs is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+
+;; GNU Emacs is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to the
+;; Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+;; Boston, MA 02110-1301, USA.
+
+;;; Code:
+
 ;;; My color theme.
 ;;; Color light and dark themes toggle
 ;;; <http://www.gnu.org/software/emacs/manual/html_node/emacs/Faces.html>.
@@ -19,6 +54,11 @@
   '((t (:inherit ido-first-match)))
   "Face used by Ido for highlighting only match."
   :group 'ido)
+
+(defface sh-heredoc
+  '((t (:inherit font-lock-doc-face)))
+  "Face to show a here-document"
+  :group 'sh-indentation)
 
 (defface helm-selection
   '((((background dark)) :background nil :underline t)
@@ -89,3 +129,5 @@
        (set-face-foreground 'success "Green4") ;Green1
        (set-face-foreground 'warning "red") ;DarkOrange
        ))
+
+;;; init-my-color-theme.el ends here
