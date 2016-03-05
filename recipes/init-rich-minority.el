d64979bc579e7dd55614b85f4e876bbc339801e4
Danil Kutkevich <danil@kutkevich.org>
2016-03-05 16:57:41 +0300

    fix: rich-minority config via custom-set-variables

--- recipes/init-rich-minority.el
+++ recipes/init-rich-minority.el
@@ -32,45 +32,47 @@
 ;; Boston, MA 02110-1301, USA.
 
 ;;; Code:
-(defvar my-highlighted-modes)
 
-(my-init--hook
-  (my-init--after-load 'rich-minority
-    (dolist (my-highlighted-modes '(
-                                    ("\\` Compiling\\'" 'display " C*") ;∞ U+221E infin or infinity
-                                    ("\\` FlyC!\\'" 'display " F!") ;flycheck-mode: The syntax check failed. Inspect the ‘*Messages*’ buffer for more information about the failure.
-                                    ("\\` FlyC*\\'" 'display " F*") ;flycheck-mode: flycheck currently checks the current buffer
-                                    ("\\` FlyC-\\'" 'display " F-") ;flycheck-mode: Flycheck did not find any syntax checker for the current buffer. Try C-c ! v (flycheck-verify-setup) to find out why.
-                                    ("\\` FlyC\?\\'" 'display " F?") ;flycheck-mode: The syntax check had a dubious result. The definition of the syntax checker may be flawed. Inspect the ‘*Messages*’ buffer for details.
-                                    ("\\` FlyC\\'" 'display " F") ;flycheck-mode: there are no errors in the current buffer
-                                    ;; ("\\` FlyC:3/5\\'" 'display " ") ;flycheck-mode: there are three errors and five warnings in the current buffer
-                                    ;; ("\\` Less\\'" 'face 'font-lock-warning-face)
-                                    ))
-      (add-to-list 'rm-text-properties my-highlighted-modes))
+(custom-set-variables
+ '(rm-blacklist '(
+                  " hl-p"
 
-    (dolist (my-hidden-modes '(
-                               " ," ;wtf!???
-                               " AC" ;auto-complete-mode
-                               " ARev" ;auto-revert-mode
-                               " Abbrev" ;abbrev-mode
-                               " Anzu" ;anzu-mode
-                               " Helm" ;helm-mode
-                               " MRev" ;magit-auto-revert-mode
-                               " Rails" ;projectile-rails
-                               " Rbow" ;rainbow-mode
-                               " RubyRef" ;ruby-refactor-mode
-                               " SP" ;smartparens-mode
-                               " Undo-Tree" ;undo-tree-mode
-                               " end" ;ruby-end-mode
-                               " ew:mnl" ;ethan-wspace find NO errors in Makefile, C or Go sources
-                               " ew:mnlt" ;ethan-wspace find NO errors (highlight trailing whitespace)
-                               " gg" ;git-gutter-mode
-                               " hl-s" ;highlight-symbol-mode
-                               " hs" ;hideshow
-                               " ing" ;indent-guide-mode
-                               " pair" ;autopair
-                               " rt" ;ruby-tools-mode
-                               " yas";yas-minor-mode aka yasnippet
-                               )) (add-to-list 'rm-blacklist my-hidden-modes))))
+                  " ," ;wtf!???
+                  " AC" ;auto-complete-mode
+                  " ARev" ;auto-revert-mode
+                  " Abbrev" ;abbrev-mode
+                  " Anzu" ;anzu-mode
+                  " Helm" ;helm-mode
+                  " MRev" ;magit-auto-revert-mode
+                  " Rails" ;projectile-rails
+                  " Rbow" ;rainbow-mode
+                  " RubyRef" ;ruby-refactor-mode
+                  " SP" ;smartparens-mode
+                  " Undo-Tree" ;undo-tree-mode
+                  " end" ;ruby-end-mode
+                  " ew:mnl" ;ethan-wspace find NO errors in Makefile, C or Go sources
+                  " ew:mnlt" ;ethan-wspace find NO errors (highlight trailing whitespace)
+                  " gg" ;git-gutter-mode
+                  " hl-s" ;highlight-symbol-mode
+                  " hs" ;hideshow
+                  " ing" ;indent-guide-mode
+                  " pair" ;autopair
+                  " rt" ;ruby-tools-mode
+                  " yas";yas-minor-mode aka yasnippet
+                  ))
+
+ '(rm-text-properties '(
+                        ("\\` Ovwrt\\'" 'face 'font-lock-warning-face)
+
+                        ("\\` Compiling\\'" 'display " C*") ;∞ U+221E infin or infinity
+                        ("\\` FlyC!\\'" 'display " F!") ;flycheck-mode: The syntax check failed. Inspect the ‘*Messages*’ buffer for more information about the failure.
+                        ("\\` FlyC*\\'" 'display " F*") ;flycheck-mode: flycheck currently checks the current buffer
+                        ("\\` FlyC-\\'" 'display " F-") ;flycheck-mode: Flycheck did not find any syntax checker for the current buffer. Try C-c ! v (flycheck-verify-setup) to find out why.
+                        ("\\` FlyC\?\\'" 'display " F?") ;flycheck-mode: The syntax check had a dubious result. The definition of the syntax checker may be flawed. Inspect the ‘*Messages*’ buffer for details.
+                        ("\\` FlyC\\'" 'display " F") ;flycheck-mode: there are no errors in the current buffer
+                        ("\\` ↑\\'" 'face 'warning) ;modalka-mode
+                        ;; ("\\` FlyC:3/5\\'" 'display " ") ;flycheck-mode: there are three errors and five warnings in the current buffer
+                        ;; ("\\` Less\\'" 'face 'font-lock-warning-face)
+                        )))
 
 ;;; init-rich-minority.el ends here
