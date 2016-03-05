22282148849f8c2bac3ff5947d911693a4d771e2
Danil Kutkevich <danil@kutkevich.org>
2016-03-05 14:48:41 +0300

    fix: window-numbering-mode default keybindings

--- recipes/init-window-numbering.el
+++ recipes/init-window-numbering.el
@@ -1,24 +1,72 @@
+;;; init-window-numbering.el --- This file is part of Danil <danil@kutkevich.org> home.
+
+;; Copyright (C) 2016 Danil <danil@kutkevich.org>.
+;; Author: Danil <danil@kutkevich.org>
+;; Version: 0.0.1
+;; Package-Requires: ((my-init))
+;; Keywords: convenience
+;; URL: https://github.com/danil/init.el
+
+;;; Commentary:
+
+;; Please see README.md for documentation.
+
+;;; License:
+
+;; This file is not part of GNU Emacs.
+;; However, it is distributed under the same license.
+
+;; GNU Emacs is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+
+;; GNU Emacs is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to the
+;; Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+;; Boston, MA 02110-1301, USA.
+
+;;; Code:
+
 (autoload 'window-numbering-mode "window-numbering"
   "Numbered window shortcuts" t)
 
+(custom-set-variables '(window-numbering-format "%s "))
+
 (my-init--hook
+  (window-numbering-mode)
+
   (my-init--after-load 'window-numbering
     ;; ;; <http://blog.binchen.org/?p=512#sec-1>.
     ;; (custom-set-faces
     ;;  '(window-numbering-face
     ;;    ((t (:foreground "brightmagenta" :background nil :weight normal :underline nil)))))
 
-    (global-set-key (my-kbd "0") 'select-window-0)
-    (global-set-key (my-kbd "1") 'select-window-1)
-    (global-set-key (my-kbd "2") 'select-window-2)
-    (global-set-key (my-kbd "3") 'select-window-3)
-    (global-set-key (my-kbd "4") 'select-window-4)
-    (global-set-key (my-kbd "5") 'select-window-5)
-    (global-set-key (my-kbd "6") 'select-window-6)
-    (global-set-key (my-kbd "7") 'select-window-7)
-    (global-set-key (my-kbd "8") 'select-window-8)
-    (global-set-key (my-kbd "9") 'select-window-9))
+    (define-key window-numbering-keymap "\M-0" nil)
+    (define-key window-numbering-keymap "\M-1" nil)
+    (define-key window-numbering-keymap "\M-2" nil)
+    (define-key window-numbering-keymap "\M-3" nil)
+    (define-key window-numbering-keymap "\M-4" nil)
+    (define-key window-numbering-keymap "\M-5" nil)
+    (define-key window-numbering-keymap "\M-6" nil)
+    (define-key window-numbering-keymap "\M-7" nil)
+    (define-key window-numbering-keymap "\M-8" nil)
+    (define-key window-numbering-keymap "\M-9" nil)
 
-  (window-numbering-mode)
+    (define-key window-numbering-keymap (my-kbd "0") 'select-window-0)
+    (define-key window-numbering-keymap (my-kbd "1") 'select-window-1)
+    (define-key window-numbering-keymap (my-kbd "2") 'select-window-2)
+    (define-key window-numbering-keymap (my-kbd "3") 'select-window-3)
+    (define-key window-numbering-keymap (my-kbd "4") 'select-window-4)
+    (define-key window-numbering-keymap (my-kbd "5") 'select-window-5)
+    (define-key window-numbering-keymap (my-kbd "6") 'select-window-6)
+    (define-key window-numbering-keymap (my-kbd "7") 'select-window-7)
+    (define-key window-numbering-keymap (my-kbd "8") 'select-window-8)
+    (define-key window-numbering-keymap (my-kbd "9") 'select-window-9)))
 
-  (custom-set-variables '(window-numbering-format "%s ")))
+;;; init-window-numbering.el ends here
