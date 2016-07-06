91610429d52c96e5345285b09be7f27f3de4261f
Danil Kutkevich <danil@kutkevich.org>
2016-07-06 20:05:45 +0300

    minor

--- recipes/init-bash-completion.el
+++ recipes/init-bash-completion.el
@@ -1,3 +1,46 @@
+;;; init-bash-completion.el --- This file is part of Danil <danil@kutkevich.org> home.
+
+;; Copyright (C) 2014-2016 Danil <danil@kutkevich.org>.
+;; Author: Danil <danil@kutkevich.org>
+;; Version: 0.0.1
+;; Package-Requires: ((my-init))
+;; Keywords: convenience
+;; URL: https://github.com/danil/init.el
+
+;;; Commentary:
+
+;; Please see README.md for documentation.
+
+;;; License:
+
+;; This file is not part of GNU Emacs.
+;; However, it is distributed under the same license.
+
+;; GNU Emacs is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+
+;; GNU Emacs is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to the
+;; Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+;; Boston, MA 02110-1301, USA.
+
+;;; Code:
+
+;; ;; For better jsx syntax-highlighting in web-mode
+;; ;; - courtesy of Patrick @halbtuerke.
+;; (defadvice web-mode-highlight-part (around tweak-jsx activate)
+;;   (if (equal web-mode-content-type "jsx")
+;;       (let ((web-mode-enable-part-face nil))
+;;         ad-do-it)
+;;     ad-do-it))
+
 (my-init--hook
   (autoload 'bash-completion-dynamic-complete "bash-completion"
     "BASH completion hook")
@@ -7,3 +50,5 @@
 
   (add-hook 'shell-command-complete-functions
             'bash-completion-dynamic-complete))
+
+;;; init-bash-completion.el ends here
