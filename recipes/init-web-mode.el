96aa2d7e7a5b2d3bda2f19640eb23754eafbcb47
Danil Kutkevich <danil@kutkevich.org>
2016-07-07 12:15:16 +0300

    minor

--- recipes/init-web-mode.el
+++ recipes/init-web-mode.el
@@ -33,6 +33,14 @@
 
 ;;; Code:
 
+;; ;; For better jsx syntax-highlighting in web-mode
+;; ;; - courtesy of Patrick @halbtuerke.
+;; (defadvice web-mode-highlight-part (around tweak-jsx activate)
+;;   (if (equal web-mode-content-type "jsx")
+;;       (let ((web-mode-enable-part-face nil))
+;;         ad-do-it)
+;;     ad-do-it))
+
 (my-init--hook
  (my-init--add-mode-to-patterns 'web-mode
                                 "/src/vendor/narus/narus-web/.+\\.jsx?\\'"
