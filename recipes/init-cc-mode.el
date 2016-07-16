395a2540ef95eb0362d84fd28314d6f963234783
Danil Kutkevich <danil@kutkevich.org>
2016-07-16 14:48:40 +0300

    minor

--- recipes/init-cc-mode.el
+++ recipes/init-cc-mode.el
@@ -37,10 +37,10 @@
   "Init."
 
   (my-init--add-mode-to-patterns 'c-mode
-                                "/etc/portage/savedconfig/www-servers/quark"
-                                "/etc/portage/savedconfig/x11-misc/dmenu"
-                                "/etc/portage/savedconfig/x11-misc/tabbed"
-                                "/etc/portage/savedconfig/x11-terms/st"
-                                "/etc/portage/savedconfig/x11-wm/dwm"))
+                                 "/etc/portage/savedconfig/www-servers/quark"
+                                 "/etc/portage/savedconfig/x11-misc/dmenu"
+                                 "/etc/portage/savedconfig/x11-misc/tabbed"
+                                 "/etc/portage/savedconfig/x11-terms/st"
+                                 "/etc/portage/savedconfig/x11-wm/dwm"))
 
 ;;; init-cc-mode.el ends here
