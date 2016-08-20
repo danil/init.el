f2f7a1c1c90fb17e08983b8ed94f4a2901ffb5d0
Danil Kutkevich <danil@kutkevich.org>
2016-08-20 22:27:19 +0300

    fix: smex history file

--- recipes/init-smex.el
+++ recipes/init-smex.el
@@ -31,12 +31,21 @@
 
 ;;; Code:
 
+(custom-set-variables
+ '(smex-history-length 5000)
+ '(smex-save-file  "~/.emacs.var/smex-items"))
+
 (global-set-key (kbd "M-x") 'smex)
 (global-set-key (kbd "M-X") 'smex-major-mode-commands)
 (global-set-key (kbd "C-c C-c M-x") 'execute-extended-command) ;this is your old M-x
 
-(my-init--after-load 'smex
-  (setq smex-key-advice-ignore-menu-bar t) ;<http://stackoverflow.com/questions/19781529/how-to-disable-emacs-messages-like-you-can-run-the-command-x-with-y#comment-29432155>
-  (setq smex-history-length 200))
+(add-hook 'after-init-hook 'init-smex)
+
+(defun init-smex ()
+  "Init."
+
+  (my-init--after-load 'smex
+    ;; <http://stackoverflow.com/questions/19781529/how-to-disable-emacs-messages-like-you-can-run-the-command-x-with-y#comment-29432155>
+    (setq smex-key-advice-ignore-menu-bar t)))
 
 ;;; init-smex.el ends here
