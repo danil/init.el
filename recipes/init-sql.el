745f038fe531d22d586d71edad53643cc6b3dfb0
Danil Kutkevich <danil@kutkevich.org>
2016-08-21 11:08:33 +0300

    save sql inferior history in conventional dir

--- recipes/init-sql.el
+++ recipes/init-sql.el
@@ -65,9 +65,8 @@ Clearing by `delete-region' or by `kill-region' if `ARG'."
         (rval 'sql-product))
     (if (symbol-value rval)
         (let ((filename
-               (concat "~/.emacs.d/sql/"
-                       (symbol-name (symbol-value rval))
-                       "-history.sql")))
+               (format "~/.emacs.var/sql/%s-history"
+                    (symbol-name (symbol-value rval)))))
           (set (make-local-variable lval) filename))
       (error
        (format "SQL history will not be saved because %s is nil"
