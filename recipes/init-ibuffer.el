bcf7153b26a2f1c96985154a5c376239deb738c8
Danil Kutkevich <danil@kutkevich.org>
2016-08-29 14:11:28 +0300

    fix: ibuffer respects goto-line hotkey

--- recipes/init-ibuffer.el
+++ recipes/init-ibuffer.el
@@ -47,6 +47,7 @@
   (my-init--after-load 'ibuffer
     ;; (setq ibuffer-use-other-window 1)
     ;; <http://stackoverflow.com/questions/7598433/how-to-remove-a-key-from-a-minor-mode-keymap-in-emacs#7598754>.
+    (define-key ibuffer-mode-map "\M-g" nil) ;ibuffer-jump-to-buffer
     (define-key ibuffer-mode-map "\M-n" nil) ;unset ibuffer-forward-filter-group
     (define-key ibuffer-mode-map "\M-p" nil) ;unset ibuffer-backward-filter-group
 
