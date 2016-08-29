fb70e74485abba258fc28dc395287a01cab32361
Danil Kutkevich <danil@kutkevich.org>
2016-08-29 14:01:08 +0300

    add my-kill-emacs hotkey

--- recipes/init-kill-emacs.el
+++ recipes/init-kill-emacs.el
@@ -1,3 +1,43 @@
+;;; init-kill-emacs.el --- This file is part of Danil <danil@kutkevich.org> home.
+
+;; Copyright (C) 2016 Danil <danil@kutkevich.org>.
+;; Author: Danil <danil@kutkevich.org>
+;; Maintainer: Danil <danil@kutkevich.org>
+;; URL: https://github.com/danil/init.el
+
+;;; Commentary:
+
+;; Please see README.md for documentation.
+
+;;; License:
+
+;; This file is not part of GNU Emacs.
+;; However, it is distributed under the same license.
+
+;; GNU Emacs is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+
+;; GNU Emacs is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to the
+;; Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+;; Boston, MA 02110-1301, USA.
+
+;;; Code:
+
+(add-hook 'after-init-hook 'init-kill-emacs)
+
+(defun init-kill-emacs ()
+  "Init."
+
+  (define-key myinit-exec-map (kbd "k") 'my-kill-emacs))
+
 (defun my-kill-emacs ()
   (interactive)
 
@@ -10,3 +50,5 @@
   (when save-place-loaded (save-place-alist-to-file))
 
   (kill-emacs))
+
+;;; init-kill-emacs.el ends here
