20c3081f557c64a0f243e0871d1e5630054446fa
Danil Kutkevich <danil@kutkevich.org>
2016-09-22 20:00:25 +0300

    minor

--- recipes/init-indent.el
+++ recipes/init-indent.el
@@ -1,6 +1,41 @@
+;;; init-indent.el --- This file is part of Danil <danil@kutkevich.org> home.
+
+;; Copyright (C) 2016 Danil <danil@kutkevich.org>.
+;; Author: Danil <danil@kutkevich.org>
+;; Maintainer: Danil <danil@kutkevich.org>
+;; URL: https://github.com/danil/init.el
+
+;;; Commentary:
+
+;; Please see README.md for documentation.
+
+;;; License:
+
+;; This file is not part of GNU Emacs.
+;; However, it is distributed under the same license.
+
+;; GNU Emacs is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+
+;; GNU Emacs is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to the
+;; Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+;; Boston, MA 02110-1301, USA.
+
+;;; Code:
+
 ;; <http://stackoverflow.com/questions/69934/set-4-space-indent-in-emacs-in-text-mode>.
 (custom-set-variables '(default-tab-width 2)
                       '(indent-tabs-mode nil)
                       '(standard-indent 2)
                       '(tab-stop-list (number-sequence 2 200 2))
                       '(tab-width 2))
+
+;;; init-indent.el ends here
