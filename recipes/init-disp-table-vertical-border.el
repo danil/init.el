105a2d04247cea12da200bef5b779f5aa0a87047
Danil Kutkevich <danil@kutkevich.org>
2016-10-21 09:47:46 +0300

    set window vertical-border to zero

--- recipes/init-disp-table-vertical-border.el
+++ recipes/init-disp-table-vertical-border.el
@@ -32,11 +32,12 @@
 ;;; Code:
 
 ;; Set whitespace as vertical separator character symbol
-;; <http://stackoverflow.com/questions/18210631/how-to-change-the-character-composing-the-emacs-vertical-border#18211568>.
+;; <http://stackoverflow.com/questions/18210631/how-to-change-the-character-composing-the-emacs-vertical-border#18211568>,
+;; <https://lists.gnu.org/archive/html/help-gnu-emacs/2013-01/msg00271.html>
 (unless standard-display-table
   (setq standard-display-table (make-display-table)))
 
 (set-display-table-slot standard-display-table
-                        'vertical-border (make-glyph-code 32)) ; ?\u200B zero width space ;(make-glyph-code 32) ;?|))
+                        'vertical-border (make-glyph-code 8203)) ; ?\u200B is an zero width space ;(make-glyph-code 32) is an blank space ;?|))
 
 ;;; init-disp-table-vertical-border.el ends here
