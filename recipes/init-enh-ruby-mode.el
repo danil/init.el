ba7324025fbf9473a5543913973eef3d9fd59a2b
Danil Kutkevich <danil@kutkevich.org>
2018-10-31 12:37:33 +0300

    minor

--- recipes/init-enh-ruby-mode.el
+++ recipes/init-enh-ruby-mode.el
@@ -31,13 +31,15 @@
 
 ;;; Code:
 
-(add-hook 'after-init-hook 'myinit-enh-ruby-mode)
-(defun myinit-enh-ruby-mode ()
-  "My init."
-  (add-to-list 'auto-mode-alist '("\\.rb\\'" . enh-ruby-mode))
-  (add-to-list 'interpreter-mode-alist '("\\.ruby\\'" . enh-ruby-mode))
-  (dolist (pattern myinit-ruby-mode-patterns)
-    (add-to-list 'auto-mode-alist (cons pattern 'enh-ruby-mode))))
+;; (add-hook 'after-init-hook 'myinit-enh-ruby-mode)
+;; (defun myinit-enh-ruby-mode ()
+;;   "My init."
+;;   ;; (setq enh-ruby-deep-indent-paren nil)
+;;   ;; (setq enh-ruby-bounce-deep-indent t)
+;;   (add-to-list 'auto-mode-alist '("\\.rb\\'" . enh-ruby-mode))
+;;   (add-to-list 'interpreter-mode-alist '("\\.ruby\\'" . enh-ruby-mode))
+;;   (dolist (pattern myinit-ruby-mode-patterns)
+;;     (add-to-list 'auto-mode-alist (cons pattern 'enh-ruby-mode))))
 
 (defun myinit-enh-ruby-mode--rainbow-identifiers-init ()
   (when (equal major-mode 'enh-ruby-mode)
