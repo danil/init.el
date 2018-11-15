7e71a2bdb3ea52e4251b4bc912898afdf306437b
Danil Kutkevich <danil@kutkevich.org>
2018-11-15 13:11:54 +0300

    fix: fixing one more time mode line color calculation

--- recipes/init-mode-line.el
+++ recipes/init-mode-line.el
@@ -54,27 +54,23 @@
                         )))  ;; :inherit shadow :background gray30
  )
 
-;; (add-hook 'after-init-hook 'myinit-mode-line)
-;; (defun myinit-mode-line ()
-;;   "My init."
-;;   ;; (remove-hook 'window-configuration-change-hook 'myinit-mode-line--face-mode-line-update-lazy)
-;;   ;; (remove-hook 'minibuffer-setup-hook 'myinit-mode-line--face-mode-line-update-lazy)
-;;   ;; (add-hook 'minibuffer-setup-hook 'myinit-mode-line--face-mode-line-update-lazy)
-;;   ;; (add-hook 'window-configuration-change-hook 'myinit-mode-line--face-mode-line-update-lazy)
-;;   ;; (add-hook 'window-setup-hook 'myinit-mode-line--face-mode-line-update-lazy)
-;;   ;; (add-hook 'auto-save-hook 'myinit-mode-line--face-mode-line-update-lazy)
-;;   )
+(add-hook 'after-init-hook 'myinit-mode-line)
+(defun myinit-mode-line ()
+  "My init."
+  ;; (remove-hook 'window-configuration-change-hook 'myinit-mode-line--face-mode-line-update-lazy)
+  (add-hook 'window-configuration-change-hook 'myinit-mode-line--face-mode-line-update-lazy))
 
-;; ;; (defun myinit-mode-line--face-mode-line-update-lazy ()
-;; ;;   (run-with-idle-timer 0.1 t (lambda ()
-;; ;;                                (myinit-mode-line--face-mode-line-update))))
-;; ;; (defun myinit-mode-line--face-mode-line-update ()
-;; ;;   (unless (active-minibuffer-window))
-;; ;;   (if (equal (count-windows) 1)
-;; ;;       (custom-set-faces
-;; ;;        '(mode-line ((((class color) (min-colors 88) (background dark)) (:background "black")))))
-;; ;;     (custom-set-faces
-;; ;;      '(mode-line ((((class color) (min-colors 88) (background dark)) (:background "MidnightBlue")))))))
+(defun myinit-mode-line--face-mode-line-update-lazy ()
+  (run-with-idle-timer 0.1 nil (lambda ()
+                                 (myinit-mode-line--face-mode-line-update))))
+
+(defun myinit-mode-line--face-mode-line-update ()
+  ;; (unless (active-minibuffer-window))
+  (if (equal (count-windows) 1)
+      (custom-set-faces
+       '(mode-line ((((class color) (min-colors 88) (background dark)) (:background "black")))))
+    (custom-set-faces
+     '(mode-line ((((class color) (min-colors 88) (background dark)) (:background "MidnightBlue")))))))
 
 ;; ;; (defun myinit-mode-line--count-windows (&optional minibuf)
 ;; ;;   "Return the number of visible windows.
