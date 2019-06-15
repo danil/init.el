72a0bfafc881cae6ee77021eae6bc9ec89eca684
Danil Kutkevich <danil@kutkevich.org>
2019-06-15 15:45:51 +0300

    minor

--- recipes/init-kill-ring.el
+++ recipes/init-kill-ring.el
@@ -37,7 +37,7 @@
 (defun myinit-kill-ring () "My init.")
 
 ;; <http://superuser.com/questions/546619/clear-the-kill-ring-in-emacs#546627>.
-(defun my-kill-ring-clear ()
+(defun myinit-kill-ring--clear ()
   (interactive)
   (progn (setq kill-ring nil) (garbage-collect)))
 
