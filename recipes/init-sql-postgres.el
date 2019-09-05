91848f0e38743814df56da1c53f794324cebc72f
Danil Kutkevich <danil@kutkevich.org>
2019-09-05 21:24:01 +0300

    fix: prompt for the postgresql

--- recipes/init-sql-postgres.el
+++ recipes/init-sql-postgres.el
@@ -50,6 +50,6 @@
   ;; <https://unix.stackexchange.com/questions/233518/mariadb-client-has-no-prompt-in-emacs-sql-mode>.
   ;; (sql-set-product-feature 'postgres :prompt-cont-regexp "^\[a-zA-Z_\]*\[-(\]\[#>\] ")
   ;; (sql-set-product-feature 'postgres :prompt-length 20)
-  (sql-set-product-feature 'postgres :prompt-regexp "^\[a-zA-Z0-9_\]*\[=-\]# "))
+  (sql-set-product-feature 'postgres :prompt-regexp "^\[a-zA-Z0-9_\]*\[=-\]\[#>\] "))
 
 ;;; init-sql-postgres.el ends here
