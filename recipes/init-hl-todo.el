18754f9b868c209e303b204c1ff0c0de7c8d1ae8
Danil Kutkevich <danil@kutkevich.org>
2020-09-01 11:01:53 +0300

    minor

--- recipes/init-hl-todo.el
+++ recipes/init-hl-todo.el
@@ -38,6 +38,9 @@
 (defun myinit-hl-todo ()
   "My init."
 
+  ;; (if (boundp 'hl-todo-mode-map) (myinit-hl-todo--init)
+  ;;   (with-eval-after-load 'hl-todo (myinit-hl-todo--init)))
+
   (dolist (hook myinit-programming-modes-hooks)
     (add-hook hook 'myinit-hl-todo--lazyinit)))
 
@@ -47,5 +50,9 @@
   (myinit-run-with-idle-timer-in-current-buffer
    myinit-default-idle-timer-seconds nil
    (lambda () (hl-todo-mode))))
+
+;; (defun myinit-hl-todo--init ()
+;;   (setq hl-todo-keyword-faces
+;;         (append hl-todo-keyword-faces '(("FOO" . "#cc9393")))))
 
 ;;; init-hl-todo.el ends here
