7a8a553b331086806c18ea12a84bb7e2df951dc3
Danil Kutkevich <danil@kutkevich.org>
2020-10-05 17:37:06 +0300

    fixing xsel

--- recipes/init-interprogram.el
+++ recipes/init-interprogram.el
@@ -1,3 +1,36 @@
+;;; init-interprogram.el --- This file is part of Danil <danil@kutkevich.org> home.
+
+;; Copyright (C) 2020 Danil <danil@kutkevich.org>.
+;; Author: Danil <danil@kutkevich.org>
+;; Maintainer: Danil <danil@kutkevich.org>
+;; URL: https://github.com/danil/init.el
+
+;;; Commentary:
+
+;; Please see README.md for documentation.
+
+;;; License:
+
+;; This file is not part of GNU Emacs.
+;; However, it is distributed under the same license.
+
+;; GNU Emacs is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+
+;; GNU Emacs is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to the
+;; Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+;; Boston, MA 02110-1301, USA.
+
+;;; Code:
+
 (add-hook 'after-init-hook 'noxrcp-interprogram)
 
 (defun noxrcp-interprogram ()
@@ -25,7 +58,11 @@
         ;; from the top of the kill-ring (car kill-ring), then return
         ;; it. Else, nil is returned, so whatever is in the top of the
         ;; kill-ring will be used.
-        (let ((xsel-output (shell-command-to-string "xsel --clipboard --output")))
+        (let ((xsel-output (shell-command-to-string
+                            ;; "2> /dev/null" is a workaround
+                            ;; for the "\ No newline at end of selection" error
+                            ;; <https://unix.stackexchange.com/questions/337171/to-which-stream-does-xsel-print-the-no-newline-warning#337335>.
+                            "xsel --clipboard --output 2> /dev/null")))
           (unless (string= (car kill-ring) xsel-output)
             xsel-output )))
       ;; Attach callbacks to hooks.
@@ -35,3 +72,5 @@
       ;; http://shreevatsa.wordpress.com/2006/10/22/emacs-copypaste-and-x/
       ;; http://www.mail-archive.com/help-gnu-emacs@gnu.org/msg03577.html
       )))
+
+;;; init-selectrumy.el ends here
