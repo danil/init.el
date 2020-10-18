896da606ed6deee2a223be999631b3ac076ee827
Danil Kutkevich <danil@kutkevich.org>
2020-10-18 21:54:14 +0300

    refactoring conf mode recipe

--- recipes/init-conf-mode.el
+++ recipes/init-conf-mode.el
@@ -1,6 +1,6 @@
 ;;; init-conf-mode.el --- This file is part of Danil <danil@kutkevich.org> home.
 
-;; Copyright (C) 2017 Danil <danil@kutkevich.org>.
+;; Copyright (C) 2020 Danil <danil@kutkevich.org>.
 ;; Author: Danil <danil@kutkevich.org>
 ;; Maintainer: Danil <danil@kutkevich.org>
 ;; URL: https://github.com/danil/init.el
@@ -37,93 +37,94 @@
 
 (custom-set-variables '(noxrcp-conf-mode-patterns
                         '(
-                          "/.config/sxhkd/"
-                          "/Pipfile2\\'"
-                          "/Pipfile3\\'"
-                          "/Pipfile\\'"
-                          "/\\.config/skippy-xd/skippy-xd\\.rc\\'"
-                          "/\\.curlrc\\'"
-                          "/\\.gtkrc-2.0\\'"
-                          "/\\.inputrc\\'"
-                          "/\\.moc/keymap\\'"
-                          "/\\.npmrc\\'"
-                          "/\\.offlineimaprc\\'"
-                          "/\\.rvmrc\\'"
-                          "/\\.screenrc\\'"
-                          "/\\.tigrc\\'"
-                          "/conkyrc_calendar\\'"
-                          "/conkyrc_top\\'"
-                          "/csf.allow\\'"
-                          "/dunstrc\\'"
-                          "/etc/aliases\\'"
-                          "/etc/fstab\\'"
-                          "/etc/mail/aliases\\'"
-                          "/etc/mailutils\\.d/"
-                          "/etc/sudoers\\.tmp\\'"
-                          "/fail2ban/.*\\.local\\'"
-                          "/hosts\\'"
-                          "/htoprc\\'"
-                          "/inventor\\(y\\|ies\\)/[^.]*\\'"
-                          "/locale.gen\\'"
-                          "/mailutils\\.rc\\'"
-                          "/mimeapps.list\\'"
-                          "/monit.d/[^/]*\\'"
-                          "/monitrc\\'"
-                          "/robots.txt\\'"
-                          "/securetty\\'"
-                          "/share/applications/defaults\\.list\\'"
-                          "/sshd_config\\'"
-                          "/sudoers\\'"
-                          "/tint2rc\\'"
-                          "\\.cnf\\'"
-                          "\\.eixrc\\'"
-                          "\\.features\\'" ;cucumber features
-                          "\\.feed2imaprc\\'"
-                          "\\.gtkrc.mine\\'"
-                          "\\.pkla\\'"
-                          "\\.skippyrc\\'"
-                          "\\.stalonetrayrc\\'"
-                          "\\.theme\\'"
-                          "\\.xxkbrc\\'"
-                          "\\parcelliterc\\'"
-                          "\\torrc\\'"
+                          ("/Pipfile2\\'"                           conf-mode)
+                          ("/Pipfile3\\'"                           conf-mode)
+                          ("/Pipfile\\'"                            conf-mode)
+                          ("/\\.Xmodmap\\'"                         conf-xdefaults-mode)
+                          ("/\\.config/skippy-xd/skippy-xd\\.rc\\'" conf-mode)
+                          ("/\\.config/sxhkd/"                      conf-mode)
+                          ("/\\.curlrc\\'"                          conf-mode)
+                          ("/\\.gtkrc-2.0\\'"                       conf-mode)
+                          ("/\\.inputrc\\'"                         conf-mode)
+                          ("/\\.moc/keymap\\'"                      conf-mode)
+                          ("/\\.npmrc\\'"                           conf-mode)
+                          ("/\\.offlineimaprc\\'"                   conf-mode)
+                          ("/\\.rvmrc\\'"                           conf-mode)
+                          ("/\\.screenrc\\'"                        conf-mode)
+                          ("/\\.tigrc\\'"                           conf-mode)
+                          ("/conkyrc_calendar\\'"                   conf-mode)
+                          ("/conkyrc_top\\'"                        conf-mode)
+                          ("/csf\\.allow\\'"                        conf-mode)
+                          ("/dunstrc\\'"                            conf-mode)
+                          ("/etc/aliases\\'"                        conf-mode)
+                          ("/etc/fstab\\'"                          conf-mode)
+                          ("/etc/mail/aliases\\'"                   conf-mode)
+                          ("/etc/mailutils\\.d/"                    conf-mode)
+                          ("/etc/sudoers\\.tmp\\'"                  conf-mode)
+                          ("/fail2ban/.*\\.local\\'"                conf-mode)
+                          ("/hosts\\'"                              conf-mode)
+                          ("/htoprc\\'"                             conf-mode)
+                          ("/inventor\\(y\\|ies\\)/[^.]*\\'"        conf-mode)
+                          ("/locale\\.gen.pacnew\\'"                conf-mode)
+                          ("/locale\\.gen\\'"                       conf-mode)
+                          ("/logind\\.conf\\.pacnew\\'"             conf-mode)
+                          ("/mailutils\\.rc\\'"                     conf-mode)
+                          ("/mimeapps.list\\'"                      conf-mode)
+                          ("/mirrorlist\\'"                         conf-mode)
+                          ("/mirrorlist\\.pacnew\\'"                conf-mode)
+                          ("/monit.d/[^/]*\\'"                      conf-mode)
+                          ("/monitrc\\'"                            conf-mode)
+                          ("/robots\\.txt\\'"                       conf-mode)
+                          ("/rsyncd\\.secrets\\'"                   conf-colon-mode)
+                          ("/securetty\\'"                          conf-mode)
+                          ("/share/applications/defaults\\.list\\'" conf-mode)
+                          ("/sshd_config\\'"                        conf-mode)
+                          ("/sudoers\\'"                            conf-mode)
+                          ("/tint2rc\\'"                            conf-mode)
+                          ("\\.cnf\\'"                              conf-mode)
+                          ("\\.eixrc\\'"                            conf-mode)
+                          ("\\.features\\'"                         conf-mode) ; cucumber features
+                          ("\\.feed2imaprc\\'"                      conf-mode)
+                          ("\\.gtkrc.mine\\'"                       conf-mode)
+                          ("\\.ini\\.template\\'"                   conf-unix-mode)
+                          ("\\.pgpass\\'"                           conf-colon-mode)
+                          ("\\.pkla\\'"                             conf-mode)
+                          ("\\.skippyrc\\'"                         conf-mode)
+                          ("\\.stalonetrayrc\\'"                    conf-mode)
+                          ("\\.theme\\'"                            conf-mode)
+                          ("\\.xxkbrc\\'"                           conf-mode)
+                          ("\\parcelliterc\\'"                      conf-mode)
+                          ("\\torrc\\'"                             conf-mode)
+
+                          ;; Logrotate scripts
+                          ("/etc/logrotate\\.d/" conf-mode)
+                          ("\\.logrotate\\'"     conf-mode)
+
+                          ;; Gentoo configs
+                          ("/etc/conf\\.d/"                     conf-mode)
+                          ("/etc/eixrc/"                        conf-mode)
+                          ("/etc/env\\.d/"                      conf-mode)
+                          ("/etc/fcron/"                        conf-mode)
+                          ("/etc/portage/env/"                  conf-mode)
+                          ("/etc/portage/package\\..*\\'"       conf-mode)
+                          ("/etc/portage/profile/use\\.mask\\'" conf-mode)
+                          ("/etc/portage/sets/"                 conf-mode)
+                          ("/portage/package\\.license\\'"      conf-mode)
+                          ("/profiles/categories\\'"            conf-mode)
+                          ("/profiles/package\\.mask\\'"          conf-mode)
+                          ("/profiles/repo_name\\'"             conf-mode)
+                          ("/var/lib/portage/world_sets\\'"     conf-mode)
+                          ("\\world\\'"                         conf-mode)
                           )))
 
 (add-hook 'after-init-hook 'noxrcp-conf-mode)
 (defun noxrcp-conf-mode ()
   "No X recipe init."
-  (dolist (pattern noxrcp-conf-mode-patterns)
-    (add-to-list 'auto-mode-alist (cons pattern 'conf-mode)))
 
-  ;; Logrotate scripts.
-  (dolist (pattern '("/etc/logrotate\\.d/" "\\.logrotate\\'"))
-    (add-to-list 'auto-mode-alist (cons pattern 'conf-mode)))
-
-  ;; Gentoo configs.
-  (dolist (pattern '("/etc/conf\\.d/"
-                     "/etc/eixrc/"
-                     "/etc/env\\.d/"
-                     "/etc/fcron/"
-                     "/etc/portage/env/"
-                     "/etc/portage/package\\..*\\'"
-                     "/etc/portage/profile/use\\.mask\\'"
-                     "/etc/portage/sets/"
-                     "/portage/package\\.license\\'"
-                     "/profiles/categories\\'"
-                     "/profiles/package.mask\\'"
-                     "/profiles/repo_name\\'"
-                     "/var/lib/portage/world_sets\\'"
-                     "\\world\\'"))
-    (add-to-list 'auto-mode-alist (cons pattern 'conf-mode)))
-
-  (dolist (pattern '("\\.ini\\.template\\'"))
-    (add-to-list 'auto-mode-alist (cons pattern 'conf-unix-mode)))
-
-  (dolist (pattern '("/\\.Xmodmap\\'"))
-    (add-to-list 'auto-mode-alist (cons pattern 'conf-xdefaults-mode)))
-
-  (dolist (pattern '("/rsyncd.secrets\\'" "\\.pgpass\\'"))
-    (add-to-list 'auto-mode-alist (cons pattern 'conf-colon-mode))))
+  (dolist (pattern-mode-pair noxrcp-conf-mode-patterns)
+    (let ((pattern (nth 0 pattern-mode-pair))
+          (mode (nth 1 pattern-mode-pair)))
+      (add-to-list 'auto-mode-alist (cons pattern mode)))))
 
 (defun noxrcp-conf-colon-mode--rainbow-identifiers-init ()
   (when (equal major-mode 'conf-colon-mode)
