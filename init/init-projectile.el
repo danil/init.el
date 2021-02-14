e2cae51bef55f60cef908d428292357356d6d24d
Danil Kutkevich <danil@kutkevich.org>
2021-02-14 18:16:59 +0300

    fix: starting projectile immediately

--- init/init-projectile.el
+++ init/init-projectile.el
@@ -24,15 +24,16 @@ See `projectile-register-project-type'."
 (define-minor-mode init-projectile-mode "init-projectile" nil init-projectile-mode-lighter init-projectile-mode-map
   (init-projectile-setup))
 
-(defvar init-projectile-mode-map (make-sparse-keymap) "init-projectile")
+(defun init-projectile-mode-on () "Turn `init-projectile-mode' on." (init-projectile-mode +1))
+(defun init-projectile-mode-off () "Turn `init-projectile-mode' off." (init-projectile-mode -1))
 
 (defun init-projectile ()
   (dolist (hook init-projectile-mode-hooks)
-    (add-hook hook 'init-projectile--lazy-setup-hook)))
+    (add-hook hook 'init-projectile-mode-on)))
 
 (defun init-projectile-setup ()
-  (projectile-mode +1)
   ;; (counsel-projectile-mode +1)
+  (projectile-mode +1)
 
   (set (make-local-variable 'projectile-project-root-files)
        (append projectile-project-root-files init-projectile-project-root-files))
@@ -40,9 +41,5 @@ See `projectile-register-project-type'."
   (let ((map init-projectile-mode-map)
         (prfx init-projectile-mode-prefix))
     (define-key map (kbd prfx) projectile-command-map)))
-
-(defun init-projectile--lazy-setup-hook ()
-  "Lazy setup `projectile'."
-  (init-lazy init-lazy-seconds nil 'init-projectile-mode +1))
 
 (provide 'init-projectile)
